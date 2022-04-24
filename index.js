742522e236f919eca838283754f9c6bbb21f9fcb
Sohaib Ahsan <sohaibahsan007@live.com>
2022-04-25 03:25:28 +0500

    More Udpates.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// get chart dom
+const chartDom = document.getElementById('chart');
+
+// init echart instance
+const chart = echarts.init(chartDom);
+
 window.onload = function() {
   const form = document.querySelector("form");
   form.onsubmit = OnSubmit.bind(form);
@@ -39,18 +45,47 @@ function OnSubmit(event){
     error += "Please enter your current value.\n";
   }
   if(error == ""){
-    prepareDateForProjection(formData);
+    const projectedData = prepareDateForProjection(formData);
+    drawChart(projectedData,formData);
   }else{
     alert(error);
   }
 }
-
 function prepareDateForProjection(formData){
   const startDate = new Date(formData.startDate);
   const currentDate = new Date(formData.currentDate);
-  const startValue = parseInt(formData.startValue);
-  const currentValue = parseInt(formData.currentValue);
-  const datesList = getDates(startDate, currentDate, 1000*60*60*24);
+  let datesList = getDates(startDate, currentDate, 1000*60*60*24);
+  const daysToAdd = (datesList?.length)/8;
+  currentDate.setDate(currentDate.getDate() + daysToAdd);
+  datesList = getDates(startDate, currentDate, 1000*60*60*24);
+  const startValue = parseFloat(formData.startValue);
+  const currentValue = parseFloat(formData.currentValue);
+
+  // prepare the projection list
+  let projectedList = prepareListForProjection(datesList, startValue, currentValue);
+
+  // find max value available in projected list
+  let nearestValue = projectedList[projectedList?.length-1]?.startValue;
+
+  // compare with currentValue, if the difference is greater than 1, then repeat the process by adding 25% more dates into current projected list.
+  // repeat until it is less than 1.
+  // this will give us always a projection list which has greater value than current value.
+  // which will make a good chart.
+  let difference  = currentValue - nearestValue;
+
+  if(difference < 0.1) return projectedList;
+
+  do {
+    currentDate.setDate(currentDate.getDate() + daysToAdd);
+    datesList = getDates(startDate, currentDate, 1000*60*60*24);
+    projectedList = prepareListForProjection(datesList, startValue, currentValue);
+    nearestValue = projectedList[projectedList?.length-1]?.startValue;
+    difference = currentValue - nearestValue;
+  } while (difference > 0);
+  return projectedList;
+
+}
+function prepareListForProjection(datesList, startValue, currentValue){
   const projectList = [];
   // irritate by each date and prepare the data for the chart
   for(let i=0; i<datesList.length; i++){
@@ -64,7 +99,7 @@ function prepareDateForProjection(formData){
 
     const _startValue = i == 0 ?  startValue : improvementValue;
     const _startValueNegative = i == 0 ?  startValue : declineValue;
-    const _currentValue =  i == 0 ? startValue : ((currentValue < projectList[i-1].startValue || date == null || _startValue == null) ? null : improvementValue);
+    const _currentValue =  i == 0 ? null : ((currentValue < projectList[i-1].startValue || date == null || _startValue == null) ? null : improvementValue);
     const _currentValueNegative = startValue < currentValue ? null :
     (i == 0 ? startValue : ((currentValue > projectList[i-1].startValueNegative || date == null || _startValueNegative == null) ? null : declineValue));
 
@@ -76,10 +111,141 @@ function prepareDateForProjection(formData){
       currentValue: _currentValue,
       currentValueNegative: _currentValueNegative,
       goalAchievedOnIndex: _currentValue > currentValue ? _currentValue : null,
-      goalAchievedOnIndexNegative: _currentValueNegative > currentValue ?_currentValueNegative : null,
+      goalAchievedOnIndexNegative: _currentValueNegative < currentValue ? _currentValueNegative : null,
     });
   }
+  return projectList;
+}
+function drawChart(projectedData,formData){
 
-  console.table(projectList);
-
+    const seriesSharedOption = {
+      symbol: "none",
+      symbolSize: 2,
+      type: 'line',
+      smooth: true,
+      clip: true,
+      lineStyle: {
+        width: 3,
+      },
+      emphasis: {
+        focus: "series",
+      },
+    };
+    const color = ['#5470c6', '#ee6666',  '#3ba272','#3ba272','#3ba272','#3ba272','#3ba272','#3ba272'];
+    const findCurrentProgress = projectedData?.find(item => item.goalAchievedOnIndex != null);
+    const currentProgressIndex = projectedData?.findIndex(item => item.goalAchievedOnIndex != null);
+    const findCurrentProgressNegative = projectedData?.find(item => item.goalAchievedOnIndexNegative != null);
+    const currentProgress = findCurrentProgress ?? findCurrentProgressNegative;
+    const achievementPoint = projectedData?.find(item => new Date(item?.date)?.toLocaleDateString("en-US") == new Date(formData.currentDate)?.toLocaleDateString("en-US"));
+    const symbolRotateBasedOnProgress = 1 - currentProgressIndex/projectedData?.length;
+    const markPoint ={
+      data: [
+        {
+          name: 'insideStartTop',
+          xAxis: currentProgress?.date,
+          yAxis: currentProgress?.goalAchievedOnIndex ?? currentProgress?.goalAchievedOnIndexNegative,
+          symbol: 'arrow',
+          symbolSize: 16,
+          symbolOffset: [0, 0] ,
+          symbolRotate: -100*symbolRotateBasedOnProgress,
+          itemStyle: {
+            color: '#3ba272',
+          },
+          label: {
+            formatter: 'Current Progress: ' +  formData?.currentValue,
+            position: 'left',
+            align: 'center',
+            offset: [-70, 0],
+            rotate: 0,
+            color: '#fff',
+            backgroundColor: '#3ba272',
+            padding: 6,
+            borderRadius: 6,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 5
+          },
+        },
+        {
+          name: 'insideStartTop',
+          xAxis: achievementPoint?.date,
+          yAxis: achievementPoint?.startValue,
+          symbol: 'circle',
+          symbolSize: 14,
+          itemStyle: {
+            color: '#fbbc04',
+          },
+          // label: {
+          //   formatter: parseFloat(achievementPoint?.startValue)?.toFixed(2),
+          //   position: 'end'
+          // }
+        }
+      ]
+    };
+    const markLine = {
+            data: [
+              {
+                name: 'insideStartTop',
+                xAxis: achievementPoint?.date,
+                yAxis: achievementPoint?.startValue,
+                label: {
+                  formatter: '1% \n Improvement Goal Score: ' + parseFloat(achievementPoint?.startValue)?.toFixed(2),
+                  position: 'insideMiddleTop',
+                  fontSize: 15,
+                  align: 'center',
+                  color: '#fbbc04',
+                },
+                itemStyle: {
+                  color: '#fbbc04',
+                }
+              }
+            ]
+      };
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: projectedData?.map(d => d.date),
+      },
+      legend: {
+        show: true,
+      },
+      grid: {
+        top: 30,
+        left: 200,
+        right: 60,
+        bottom: 40
+      },
+      textStyle: {
+        fontFamily: 'system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"' ,
+      },
+      yAxis:  {
+        type: 'value',
+        scale: true,
+      },
+      dataZoom: [
+        {
+          show: true,
+          type: 'inside',
+          filterMode: 'none',
+          xAxisIndex: [0],
+        },
+      ],
+      color,
+      series: [
+        {
+          data: projectedData?.map(d => d.startValue),
+          name: 'Projected 1% BETTER EVERY DAY',
+          smooth: true,
+          ...seriesSharedOption,
+          markLine,
+          markPoint
+        },
+        {
+          data: projectedData?.map(d => d.startValueNegative),
+          name: 'Projected 1% DECLINE EVERY DAY',
+          ...seriesSharedOption,
+          markPoint
+        },
+      ]
+    };
+    option && chart.setOption(option);
 }
